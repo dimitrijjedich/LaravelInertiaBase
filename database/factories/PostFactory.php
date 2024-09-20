946a51fdf09c58ce568e230f651a38e51cba3891
dimitrijjedich <dimitrij@startup-werk.de>
2024-09-20 19:13:04 +0200

    add data to PostFactory and DatabaseSeeder

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,8 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->text(30),
+            'body' => fake()->text(),
         ];
     }
 }
